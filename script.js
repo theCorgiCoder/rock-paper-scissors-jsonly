79c107e929cbba1a2153916031aa0769802e2253
Elaine B <elainebergstrom26@gmail.com>
2023-03-10 14:27:56 +0100

    code clean up
    function rework and code clean up for simplification. Added reset option

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
+// Global variables
+let playerScore = 0;
+let computerScore = 0;
+let ties = 0;
+let rounds = 0;
+
+// Randomizes computer selection
 function computerPlay() {
   const gameMoves = ["rock", "paper", "scissors"];
   const randomSelection =
@@ -5,49 +12,97 @@ function computerPlay() {
   return randomSelection;
 }
 
+// Prompt window input for player
 function playerPlay() {
-  let playerInput = prompt("Choose rock, paper, or scissors!").toLowerCase();
+  let playerInput = prompt(
+    "Let's play a game! Please choose rock, paper, or scissors!"
+  ).toLowerCase();
   return playerInput;
 }
 
-function game() {
-  console.clear;
-  let playerScore = 0;
-  let computerScore = 0;
-  let ties = 0;
-  const player = playerPlay();
-  const computer = computerPlay();
+// Simple round and score display
+function numberOfRounds() {
+  return console.log(
+    `Round: ${rounds},
+    Player Score: ${playerScore},
+    Computer Score: ${computerScore},
+    Ties: ${ties},`
+  );
+}
 
-  function playRound(playerSelection, computerSelection) {
-    if (playerSelection === computerSelection) {
-      ties += 1;
-      return console.log(
-        `${playerSelection} vs ${computerSelection}, it's a tie!`
-      );
-    } else if (playerSelection === "rock" && computerSelection === "scissors") {
-      playerScore += 1;
-      return console.log(
-        "Player wins! Rock breaks scissors! You're rocking it!"
-      );
-    } else if (
-      playerSelection === "scissors" &&
-      computerSelection === "paper"
-    ) {
-      playerScore += 1;
-      return console.log("Player win! Scissors cut paper! Shearing up to win!");
-    } else if (playerSelection === "paper" && computerSelection === "rock") {
-      playerScore += 1;
-      return console.log("Player wins! Paper covers rock!");
+function playRound(playerSelection, computerSelection) {
+  rounds += 1;
+  if (playerSelection === computerSelection) {
+    ties += 1;
+    console.log(`${playerSelection} vs ${computerSelection}, it's a tie!`);
+    return numberOfRounds();
+  } else if (playerSelection === "rock" && computerSelection === "scissors") {
+    playerScore += 1;
+    console.log("Player wins! Rock breaks scissors! You're rocking it!");
+    return numberOfRounds();
+  } else if (playerSelection === "scissors" && computerSelection === "paper") {
+    playerScore += 1;
+    console.log("Player wins! Scissors cut paper!");
+    return numberOfRounds();
+  } else if (playerSelection === "paper" && computerSelection === "rock") {
+    playerScore += 1;
+    console.log("Player wins! Paper covers rock!");
+    return numberOfRounds();
+  } else {
+    computerScore += 1;
+    console.log(
+      `Computer wins! ${computerSelection} beats ${playerSelection}!`
+    );
+    return numberOfRounds();
+  }
+}
+
+function game() {
+  // Allows only 5 rounds to be played.
+  for (let i = 0; i < 5; i++) {
+    const player = playerPlay();
+    const computer = computerPlay();
+    playRound(player, computer);
+  }
+
+  function playAgainCall() {
+    let playAgainInput = prompt("Do you want to play again?").toLowerCase();
+    return playAgainInput;
+  }
+
+  function resetGame(playAgainInput) {
+    if (playAgainInput === "yes") {
+      ties = 0;
+      playerScore = 0;
+      computerScore = 0;
+      rounds = 0;
+      console.clear();
+      return game();
     } else {
-      computerScore += 1;
-      return console.log(
-        `Player loss! ${computerSelection} beats ${playerSelection}`
-      );
+      return console.log("Thank you for playing!");
     }
   }
 
-  for (let i = 0; i < 5; i++) {
-    playRound(player, computer);
+  //Displays winner of game
+  if (ties + computerScore + playerScore === 5) {
+    if (ties === 5) {
+      console.log(
+        `You tied with the computer ${playerScore} vs. ${computerScore}. So close!`
+      );
+    } else if (playerScore > computerScore && ties) {
+      console.log(
+        `Congratulations! YOU WON!!! with ${playerScore} out of 5 rounds!`
+      );
+    } else {
+      console.log(
+        `You lose! Computer wins with ${computerScore} out of 5 rounds. Better luck next time!`
+      );
+    }
+
+    if (rounds === 5) {
+      playAgain = playAgainCall();
+      return resetGame(playAgain);
+    }
   }
 }
 
